6b21f7fd77614d2753442e6a2dde8ce492399ac2
Sarojkatwal <katwalsaroj11@gmail.com>
2019-07-10 18:05:50 +0545

    Add project files.

---
+++ DSA1/graphics.h
@@ -0,0 +1,134 @@
+template<class T>
+class INSERTDELETE
+{
+public:
+	void show(T arr[],int fr,int re,int max,int i)
+	{
+		sf::Vector2f initarrpos1,initarrpos2;
+		int j = 0;
+		float x = 10.0f, y = 110.0f, length = 950.0f / (max + 1), height = 90.0f;
+	    std::string initi = "Initially";
+		int textsize = static_cast<int>(length/3);
+		sf::ContextSettings settings;
+		settings.antialiasingLevel = 8;
+		sf::RenderWindow window(sf::VideoMode(1000, 400), "Queues",sf::Style::Default,settings);
+		std::ostringstream ddata(ios::ate | ios::out);
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+				if (event.type = sf::Event::MouseButtonPressed)
+				{
+					if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						std::cout << event.mouseButton.x << " " << event.mouseButton.y << std::endl;
+					}
+				}
+			}
+			drawtext(&window, sf::Color::Red, sf::Vector2f(300,10), "LINEAR QUEUE", 50, sf::Text::Bold);
+			drawtext(&window, sf::Color::White, sf::Vector2f(30, 70),initi, 30, sf::Text::Bold);
+			drawrectangle(&window, sf::Color::Blue, sf::Vector2f(x+length/2 , y), 90, sf::Vector2f( height,10));
+		    float m = x+length/2;
+			for (int i =0; i <re; i++)
+			{
+				ddata.str("");
+				if(i>=fr)
+					ddata << arr[i];
+				drawtext(&window, sf::Color::Green, sf::Vector2f(m+length/3.5f, y+ (height-textsize)/2), ddata.str(), textsize, sf::Text::Bold);
+				drawrectangle(&window, sf::Color::Blue, sf::Vector2f(m + length, y), 90, sf::Vector2f(height, 10.0f));
+				m = m + length;
+			}
+			drawrectangle(&window, sf::Color::Blue, sf::Vector2f(m+length, y), 90, sf::Vector2f(height, 10.0f));
+
+			drawrectangle(&window,sf::Color::Blue, sf::Vector2f(x, y), 0, sf::Vector2f((max+1) * length, 10.0f));
+			drawrectangle(&window,sf::Color::Blue, sf::Vector2f(x, y+height), 0, sf::Vector2f((max+1) * length, 10.0f));
+			if (j == 2)
+			{
+				window.display();
+				++j;
+				sf::sleep(sf::seconds(5));
+				window.clear();
+				continue;
+			}
+			ddata.str("");
+			ddata << "front=" << fr;
+			drawtext(&window, sf::Color::Cyan, sf::Vector2f((fr)*length+x+length/1.5f, y+1.25f*height), ddata.str(), 20, sf::Text::Italic);
+			ddata.str("");
+			ddata << "rear=" << re;
+			if (fr != re)
+			{
+				drawtext(&window, sf::Color::Cyan, sf::Vector2f((re)* length + x + length / 1.5f, y + 1.25f*height), ddata.str(), 20, sf::Text::Italic);
+			}
+			else
+			{
+				drawtext(&window, sf::Color::Cyan, sf::Vector2f((re)* length + x + length / 1.5f, y + 1.5f*height), ddata.str(), 20, sf::Text::Italic);
+			}
+			drawarrow(&window, sf::Vector2f((fr)*length + x + length/1.5f, y + height/1.4f));
+			drawarrow(&window, sf::Vector2f((re)* length + x + length / 1.5f, y + height / 1.4f));
+			window.display();
+			window.clear();
+			if (j == 0||j==1)
+			{
+				sf::sleep(sf::seconds(1));
+				j++;
+				initi = (i == 0) ? "After deletion" : "After insertion";
+			}
+			if (j ==2)
+			{
+		
+				if (fr != re)
+				{
+					drawtext(&window, sf::Color::Cyan, sf::Vector2f((re)* length + x + length / 1.5f, y + 1.25f*height), ddata.str(), 20, sf::Text::Italic);
+				}
+				else
+				{
+					drawtext(&window, sf::Color::Cyan, sf::Vector2f((re)* length + x + length / 1.5f, y + 1.5f*height), ddata.str(), 20, sf::Text::Italic);
+				}
+				ddata.str("");
+				ddata << "Front=" << fr;
+				drawtext(&window, sf::Color::Cyan, sf::Vector2f((fr)*length + x + length / 1.5f, y + 1.25f*height), ddata.str(), 20, sf::Text::Italic);
+				drawarrow(&window, sf::Vector2f((fr)*length + x + length / 1.5f, y + height / 1.4f));
+				drawarrow(&window, sf::Vector2f((re)* length + x + length / 1.5f, y + height / 1.4f));
+				i == 0 ? ++fr : ++re;
+			}
+		}
+	}
+	void drawrectangle(sf::RenderWindow* window,sf::Color color,sf::Vector2f pos,float ang,sf::Vector2f size)
+	{
+		sf::RectangleShape shape;
+		shape.setFillColor(color);
+		shape.setPosition(pos);
+		shape.setRotation(ang);
+		shape.setSize(size);
+		window->draw(shape);
+	}
+	void drawtext(sf::RenderWindow *window,sf::Color color,sf::Vector2f pos,std::string ddata,int size,sf::Text::Style style)
+	{
+		sf::Font font4text;
+		sf::Text nodedata;
+		font4text.loadFromFile("roman.ttf");
+		nodedata.setFont(font4text);
+		nodedata.setCharacterSize(size);
+		nodedata.setFillColor(color);
+		nodedata.setStyle(style);
+		nodedata.setString(ddata);
+		nodedata.setPosition(pos);
+		window->draw(nodedata);
+	}
+	void drawarrow(sf::RenderWindow *window,sf::Vector2f pos)
+	{
+		sf::Texture texture;
+		if (!texture.loadFromFile("arrow.png"))
+		{
+			std::cout << "PNG image is not loaded."<<std::endl;
+		}
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+		sprite.setPosition(pos);
+		sprite.setScale(sf::Vector2f(0.03f,0.03f));
+		window->draw(sprite);
+	}
+};
---
+++ DSA1/main.cpp
@@ -0,0 +1,104 @@
+#include <SFML/Graphics.hpp>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<sstream>
+#include "graphics.h"
+using namespace std;
+
+const int MAX = 8;
+class FULL {};
+class EMPTY {};
+template<class T>
+class queqe
+{
+private:
+	int fronts, rear;
+	T arr[MAX];
+public:
+	queqe()
+	{
+		fronts = 0;
+		rear = 0;
+	}
+	void enqueue(T a)
+	{
+		if (rear == MAX)
+		{
+			throw FULL();
+		}
+		else
+		{
+			arr[rear++] = a;
+			INSERTDELETE<T> A;
+			A.show(arr, fronts, rear-1,MAX,1);
+			cout << "Pushed element:" << a << endl;
+		}
+	}
+	T dequeue()
+	{
+		if (rear == fronts)
+		{
+			throw EMPTY();
+		}
+		else
+		{
+			fronts = fronts + 1;
+			INSERTDELETE<T> A;
+			A.show(arr, fronts-1, rear, MAX,0);
+			return arr[fronts-1];
+		}
+	}
+	void printfr()
+	{
+		cout << "Front=" << fronts << "  Rear=" << rear << endl;
+	}
+};
+int main()
+{
+	queqe<int> s1;
+	int i, data;
+	while (true)
+	{
+		try
+		{
+			cout << "1.ENQUEUE\n2.DEQUEUE\n3.Exit\n";
+			cout << "Enter your choice:";
+			cin >> i;
+			if (i == 1)
+			{
+				cout << "Enter the data :";
+				cin >> data;
+				s1.enqueue(data);
+				s1.printfr();
+			}
+			else if (i == 2)
+			{
+				cout << "DEQUEUEED data:" << s1.dequeue() << endl;
+				s1.printfr();
+			}
+			else if (i == 3)
+			{
+				exit(0);
+			}
+			else
+			{
+				exit(0);
+			}
+		}
+		catch (EMPTY)
+		{
+			cout << "The queue is empty\n";
+		}
+		catch (FULL)
+		{
+			cout << "The queue is full\n";
+		}
+		cin.get();
+		cin.get();
+		system("cls");
+	}
+	return 0;
+}
+
+
